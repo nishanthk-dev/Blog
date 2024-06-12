920a64707f29a52dd7087c17fbe27ba1852f03b3
nishathk200 <nishanthk200@gmail.com>
2024-06-12 22:28:47 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const home = require("./router/routes");
+const connectDB = require("./db");
+const errorMiddleware = require("./middlewares/error_middleware");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: "POST, PUT, GET, DELETE, HEAD, PATCH",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(express.json());
+app.use("/api", home);
+app.use(errorMiddleware);
+
+const PORT = process.env.PORT || 8080;
+connectDB();
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}/`);
+});
